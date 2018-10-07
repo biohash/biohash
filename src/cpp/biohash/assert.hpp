1e58e8e8d1d2b686e1553f52a279caf6cb22e8b6
Morten Krogh <morten.krogh@amberbio.com>
2018-10-07 15:03:45 +0200

    Assert macro

--- src/cpp/biohash/assert.hpp
+++ src/cpp/biohash/assert.hpp
@@ -1,13 +1,16 @@
 #pragma once
 
+#include <stdlib.h>
+
 #define ASSERT(expression) ((expression) ? static_cast<void>(0): biohash::biohash_assert(#expression, __FILE__, __LINE__))
+
 
 namespace biohash {
 
 inline void biohash_assert(const char* expression, const char* file, long line)
 {
 	fprintf(stderr, "Assertion '%s' failed, file '%s' line '%ld'.", expression, file, line);
-	abort();
+    abort();
 }
 
 }
